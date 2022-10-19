e446092cb362dcc13660d3970dfa5fcc18144f71
Matis <mlebreton002@ensc.fr>
2022-10-19 14:32:14 +0200

    test

--- ProjetIA2022/Form1.cs
+++ ProjetIA2022/Form1.cs
@@ -15,6 +15,8 @@ namespace ProjetIA2022
     public partial class Form1 : Form
     {
 
+        // Pour test
+
         static public double[,] matrice;
         static public int nblignes;
         static public int nbcolonnes;
